e856c30ec9a4e9ad28c39bbe4f2c9e92067d2b11
Daljakim <98266834+Daljakim@users.noreply.github.com>
2022-02-21 21:33:52 +0900

    수정 조금

--- js/weather.js
+++ js/weather.js
@@ -8,7 +8,7 @@ function onGeoOk(position){
         const weatherContainer = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
         city.innerText = data.name;
-        weatherContainer.innerText = data.weather[0].main;
+        weatherContainer.innerText = `${data.weather[0].main} / ${data.main.temp}˚C`;
     }));
 }
 function onGeoError(){
